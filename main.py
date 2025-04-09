22944317e2991c9dd0330974778fd3adc269886d
Stef Kraaijeveld <117903018+Stefsk-glitch@users.noreply.github.com>
2025-04-09 15:47:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
-import tkinter as tk
+from tkinter import *
+from spectralcam.gentl.gentl import *
+from spectralcam.specim.fx10 import *
 
-from spectralcam.specim import FX10
-from spectralcam.gentl import GCSystem
+cam = None
 
-# TODO: add basic functionality to interact with the camera
+def create_window():
+    window = Tk()
+    window.geometry("600x400")
+
+    connectButton = Button(window, text="Connect FX10", command=connect_camera)
+    connectButton.pack()
+
+    quickInitButton = Button(window, text="Quick Init", command=quick_init_camera)
+    quickInitButton.pack()
+
+    window.title("FX10 Configuration App")
+
+    window.mainloop()
+
+def connect_camera():
+    global cam
+    system = GCSystem()
+    cam, intf = system.discover(FX10)
+
+def quick_init_camera():
+    global cam
+    if cam:
+        cam.quick_init()
 
 if __name__ == "__main__":
-    print("Hello world!")
+    create_window()
