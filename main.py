db6ea3656c8fb9512f109228681fe9b9a16b8413
GertieMeneer <gertjan.vat@icloud.com>
2025-04-09 14:42:30 +0200

    Add specim lib, main app with connect button
    Also requirements.txt file

--- main.py
+++ main.py
@@ -28,5 +28,24 @@ def quick_init_camera():
     if cam:
         cam.quick_init()
 
+from tkinter import *
+from lib.spectralcam.gentl.gentl import *
+from lib.spectralcam.specim.fx10 import *
+
+def create_window():
+    window = Tk()
+    window.geometry("600x300")
+
+    connectButton = Button(window, text="Connect FX10", command=connect_camera)
+    connectButton.pack()
+
+    window.title("FX10 Configuration App")
+
+    window.mainloop()
+
+def connect_camera():
+    system = GCSystem()
+    fx10, intf = system.discover(FX10)
+
 if __name__ == "__main__":
     create_window()
