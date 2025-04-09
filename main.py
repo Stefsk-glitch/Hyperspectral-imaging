5602e8628ff35317bf1d4974976160fc12209075
Stef Kraaijeveld <117903018+Stefsk-glitch@users.noreply.github.com>
2025-04-09 14:18:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-# TODO: add Tkinter lib and FX10 lib
+import tkinter as tk
+
+from spectralcam.specim import FX17
+from spectralcam.gentl import GCSystem
+
 # TODO: add basic functionality to interact with the camera
 
 if __name__ == "__main__":
