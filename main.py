80acc9e7193aa2df5060d040963bcb9b554796eb
GertieMeneer <gertjan.vat@icloud.com>
2025-04-09 14:54:18 +0200

    Add quick_init button

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from tkinter import *
 from lib.spectralcam.gentl.gentl import *
 from lib.spectralcam.specim.fx10 import *
 
+cam = None
+
 def create_window():
     window = Tk()
     window.geometry("600x300")
@@ -12,13 +14,22 @@ def create_window():
     connectButton = Button(window, text="Connect FX10", command=connect_camera)
     connectButton.pack()
 
+    quickInitButton = Button(window, text="Quick Init", command=quick_init_camera)
+    quickInitButton.pack()
+
     window.title("FX10 Configuration App")
 
     window.mainloop()
 
 def connect_camera():
+    global cam
     system = GCSystem()
-    fx10, intf = system.discover(FX10)
+    cam, intf = system.discover(FX10)
+
+def quick_init_camera():
+    global cam
+    if cam:
+        cam.quick_init()
 
 if __name__ == "__main__":
     create_window()
