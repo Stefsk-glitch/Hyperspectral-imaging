a018974e025e804da973aba0ab2e4791632c3ff1
Stef Kraaijeveld <117903018+Stefsk-glitch@users.noreply.github.com>
2025-04-09 19:54:58 +0200

    Better practice remove unnecessary use of global

--- camera_connector.py
+++ camera_connector.py
@@ -1,15 +1,15 @@
 from lib.spectralcam.gentl.gentl import *
 from lib.spectralcam.specim.fx10 import *
 
-cam = None
+class CameraConnector:
+    def __init__(self):
+        self.cam = None
+        self.intf = None
 
-class CameraConnector():
     def connect(self):
-        global cam
         system = GCSystem()
-        cam, intf = system.discover(FX10)
-    
-    def quick_init_camera():
-        global cam
-        if cam:
-            cam.quick_init()
+        self.cam, self.intf = system.discover(FX10)
+
+    def quick_init_camera(self):
+        if self.cam:
+            self.cam.quick_init()
