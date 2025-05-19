2b852ed885dd43e757a3440d24e8f8f096ab150f
GertieMeneer <gertjan.vat@icloud.com>
2025-05-19 14:23:01 +0200

    Can change settings

--- settings.py
+++ settings.py
@@ -1,8 +1,17 @@
-from tkinter import Toplevel, Frame, ttk, StringVar, Entry
-from models import camera_data
+from tkinter import Toplevel, Frame, ttk, StringVar, Entry, Label, Button
+from models import camera_data, app_context
 from lib.spectralcam.specim import FXBase
 
 def open_settings_window(master):
+    def item_selected(event):
+        selected_item = tree.focus()
+        if not selected_item:
+            return
+        values = tree.item(selected_item, 'values')
+        if len(values) == 2:
+            set_value_window(values[0], values[1])
+
+
     win = Toplevel(master)
     win.title("Settings")
     win.geometry("800x600")
@@ -20,6 +29,7 @@ def open_settings_window(master):
     tree.pack(side="left", fill="both", expand=True)
     scrollbar = ttk.Scrollbar(container, orient="vertical", command=tree.yview)
     tree.configure(yscrollcommand=scrollbar.set)
+    tree.tag_bind("click", "<<TreeviewSelect>>", item_selected)
     scrollbar.pack(side="right", fill="y")
 
     cam: FXBase = camera_data["cam"]
@@ -36,7 +46,7 @@ def open_settings_window(master):
         for item in tree.get_children():
             tree.delete(item)
         for setting, value in rows:
-            tree.insert("", "end", values=(setting, value))
+            tree.insert("", "end", values=(setting, value), tags="click")
 
     def on_search(*args):
         query = search_var.get().lower()
@@ -51,5 +61,22 @@ def open_settings_window(master):
         update_treeview(filtered)
 
     search_var.trace_add("write", on_search)
+
+    def confirm_set_value(setting, value, window):
+        try:
+            cam.set(setting, value)
+            window.destroy()
+        except Exception as error:
+            app_context["message_box"](error)
+
+    def set_value_window(setting, value):
+        win = Toplevel(master)
+        win.title("Set Value")
+        Label(win, text=setting).pack()
+        Label(win, text=value).pack()
+        entry_var = StringVar()
+        entry = Entry(win, textvariable=entry_var)
+        entry.pack()
+        Button(win, text="Save", command=lambda:confirm_set_value(setting, entry_var.get(), win)).pack()
 
     update_treeview(all_rows)
