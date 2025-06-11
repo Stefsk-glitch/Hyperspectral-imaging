a102f6a7f23ce6e32c1b67e845f74a188c93741b
GertieMeneer <gertjan.vat@icloud.com>
2025-06-11 11:15:44 +0200

    Hopefully fix handle disconnect

--- enums.py
+++ enums.py
@@ -3,7 +3,8 @@ from enum import Enum
 class Events(Enum):
     CAM_FOUND = 1,
     MULTIPLE_CAMS = 2,
-    NO_CAM = 3
+    NO_CAM = 3,
+    CAM_DISCONNECTED = 4
 
 class ConnectionState(Enum):
     DISCONNECTED = 1,
